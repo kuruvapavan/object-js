b3e992314f718db8a2f9c9a3b649b2e8e221d249
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-15 22:39:17 +0530

    updated

---
+++ object.js
@@ -0,0 +1,20 @@
+var car = {
+color:"black",
+company:"audi",
+};
+console.log(car.color)
+console.log(car)
+car.color
+
+// nested objects
+
+var student = {
+    name:"pavan",
+    age:"22",
+    marks:{
+        m1:"70",
+        m2:"60",
+    }
+};
+console.log(name)
+console.log(student.age)
